fe4b446f14b8e053531793e35e3fe0feb2b78b17
Proustibat <jennifer.proust@gmail.com>
2017-05-15 14:47:38 +0200

    fix(https://github.com/proustibat/cordova-hello-world/issues/1): babel-loader requires webpack > 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,47 @@
+const path = require('path');
+
 module.exports = {
     cache: true,
-    debug: true, // shows also which files run through traceur, good to learn and validate the `exclude`
     devtool: 'source-map',
-    entry: "./src/js/index.js",
-    output: {
-        path: __dirname + "/src",
-        filename: "/js/index.min.js"
+
+
+
+    entry: {
+        // hmr: [
+        //     'webpack-dev-server/client?http://localhost:8000',
+        //     'webpack/hot/only-dev-server'
+        // ],
+        index: path.join(__dirname, 'src', 'js', 'index.js')
     },
+    output: {
+        path: path.join(__dirname, 'www', 'js'),
+        filename: '[name].js',
+        sourceMapFilename: '[file].map',
+        devtoolModuleFilenameTemplate: 'webpack:///[resource-path]?[loaders]'
+    },
+
+    // entry: "./src/js/index.js",
+    // output: {
+    //     path: __dirname + "/src",
+    //     filename: "/js/index.min.js"
+    // },
     module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "babel",
-          query: {
-              presets: ['es2015'],
-              compact: false
-          }
-        }
-
-      ]
+        rules: [
+            {
+                test: /\.(js|jsx|es6)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    "presets": [
+                        [
+                            "es2015", {
+                            "modules": false
+                        }
+                        ]
+                    ],
+                    cacheDirectory: true
+                }
+            }
+        ]
     }
-
-}
+};
